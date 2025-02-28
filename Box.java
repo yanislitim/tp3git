5b0731fe6886f8923edd4498fcac856557e0407a
yanislitim <yanis.litim@etu.univ-orleans.fr>
2025-02-28 16:11:38 +0100

    exo9

---
+++ Box.java
@@ -0,0 +1,8 @@
+import java.util.ArrayList;
+
+class Box{
+    ArrayList<Thing> contents = new ArrayList<Thing>();
+    public void add(Thing truc){
+    this.contents.add(truc);
+    }
+}
---
+++ Thing.java
@@ -0,0 +1,6 @@
+public class Thing{
+String name;
+public Thing(String name){
+this.name = name;
+}
+}
